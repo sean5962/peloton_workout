f59bf855dc8a95fe3273797ad7483ac30f51688e
Sean Christensen <sean5962@gmail.com>
2023-01-30 15:21:12 -0800

    new menu input, zone display based on stage

--- peloton.py
+++ peloton.py
@@ -1,4 +1,5 @@
 # V1 goals:
+    # Menu that asks 1, 2, 3 for which stage to display
     # List the output required for stage 1 (half-zone), stage 2 (3/4 zone), and stage 3 (full zone)
     # Inputs don't need to be dynamic yet
 
@@ -22,6 +23,22 @@ def main():
                 # Zone 6: 230 - 288
                 # Zone 7: 288+
 
+    
+
+    # Menu loop that prompts for which zone to display
+
+    while True:
+        prompt =  int(input("Which zone do you need? "))
+        
+        if prompt == 1 or prompt == 2 or prompt == 3:
+            zone_display(prompt)
+            break
+            
+
+
+def zone_display(number):
+    #use algorithm to give top range to Zone 7 because it comes uncalculated
+
     #list of dictionaries, listing name, bottom of the range, top of the range, and target output
     zones = [
         {"name": "Zone 1", "bt_range": 0, "tp_range": 106, "tgt": 0},
@@ -33,7 +50,18 @@ def main():
         {"name": "Zone 7", "bt_range": 288, "tp_range": None, "tgt": 0}
     ]
 
-    #use algorithm to give top range to Zone 7 because it comes uncalculated
+    #initialize var to populate stage number 
+    stage = number
+
+    #stage multiplier that makes it more difficult w each stage increase
+    #initialize and then set based on parameter passed through function
+    stage_mult = 1
+    if number == 1:
+        stage_mult = .5
+    elif number == 2:
+        stage_mult = .75
+    elif number == 3:
+        stage_mult = 1
 
 
     for zone in zones:
@@ -41,12 +69,14 @@ def main():
             zone['tgt'] = int(zone['bt_range'] + zone['tp_range'] * .62)
         elif zone['name'] == 'Zone 7':
             zone['tp_range'] = int(zone['bt_range'] * 1.25)
-            zone['tgt'] = int((zone['bt_range'] + zone['tp_range']) / 2)
+            zone['tgt'] = int((zone['tp_range'] - zone['bt_range']) * stage_mult + zone["bt_range"])
         else:
-            zone['tgt'] = int((zone['bt_range'] + zone['tp_range']) / 2)
+            zone['tgt'] = int((zone['tp_range'] - zone['bt_range']) * stage_mult + zone["bt_range"])
 
-        print(f"{zone['name']} (Stage 1): {zone['tgt']}")
+        print(f"{zone['name']} (Stage {stage}): {zone['tgt']}")
 
+
+    
 
     
     #testing commit
